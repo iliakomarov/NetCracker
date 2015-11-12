df5a005fa2b8ab160c251a9ada43a426541b6f99
Ilia Komarov <nexen505@yandex.ru>
2015-11-12 11:47:43 +0400

    Добавил ваши версии

---
+++ Client.java
@@ -0,0 +1,23 @@
+package client;
+
+/**
+ * Created by Fadeev on 07.11.2015.
+ */
+public interface Client {
+    boolean logIn(String login, String password);
+    boolean logOut(String login);
+    void getTree();
+    boolean load(String name);
+    boolean save(String name);
+    boolean create(String name);
+    boolean delete(String name);
+    boolean addTask(String name);
+    boolean deleteTask(String name);
+    boolean startTask(String name);
+    boolean stopTask(String name);
+    boolean pauseTask(String name);
+    boolean renameTask(String oldName, String newName);
+    void getStat();
+
+
+}
